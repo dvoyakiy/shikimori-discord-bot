7c9054c90811317b562e7b779684a9e0aa060df4
dvoyakiy <dvoyakiyyy@gmail.com>
2020-04-14 19:03:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShikimoriDiscordBot {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

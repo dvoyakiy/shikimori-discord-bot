c9591a498f59061edb0a0467d7671cac1e8be769
dvoyakiy <dvoyakiyyy@gmail.com>
2020-05-04 14:22:02 +0300

    Add DatabaseManager

---
+++ Database/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ShikimoriDiscordBot.Database.Models {
+    public class User {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string ClientId { get; set; }
+        public string Nickname { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿namespace ShikimoriDiscordBot {
+﻿using System;
+using System.Linq;
+using ShikimoriDiscordBot.Database;
+using ShikimoriDiscordBot.Database.Models;
+
+namespace ShikimoriDiscordBot {
     class Program {
         static void Main(string[] args) {
-            Bot.RunAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();  
+            //Bot.RunAsync(args).ConfigureAwait(false).GetAwaiter().GetResult(); 
         }
     }
 }
